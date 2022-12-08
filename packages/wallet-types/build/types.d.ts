5ea12070836838ff16cf06f54b0da8d5d0e3d630
Stream Protocol / StreamPay <105049869+stream-protocol@users.noreply.github.com>
2022-12-08 11:33:07 +0200

    Update types.d.ts

--- packages/wallet-types/build/types.d.ts
+++ packages/wallet-types/build/types.d.ts
@@ -1,6 +1,6 @@
 import { Keplr } from "@keplr-wallet/types";
 import { UmbralApi } from "@stream-wallet/umbral-types";
-export interface FetchBrowserWallet {
+export interface StreamBrowserWallet {
     readonly version: string;
     readonly umbral: UmbralApi;
     readonly keplr: Keplr;
